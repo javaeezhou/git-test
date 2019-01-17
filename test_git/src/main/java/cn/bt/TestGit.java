c6dc437f62f7006bdf8d277582747b5f393d98b8
zhou <zhou@qq.com>
2019-01-17 08:51:31 +0800

    将本地工程提交都git

---
+++ test_git/src/main/java/cn/bt/TestGit.java
@@ -0,0 +1,14 @@
+package cn.bt;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("你真的很帅");
+        System.out.println("你真的很帅");
+        System.out.println("你真的很帅");
+        System.out.println("你真的很帅");
+        System.out.println("你真的很帅");
+        System.out.println("你真的很帅");
+        System.out.println(" 你真的很帅");
+    }
+}
