e4ee1463c72ad7b259432ad6ceded07b278aefe6
zhou <zhou@qq.com>
2019-01-17 13:37:26 +0800

    提交java项目

---
+++ git-test2/src/cn/bt/Test.java
@@ -0,0 +1,14 @@
+package cn.bt;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("我一路向北,离开有你的季节");
+        System.out.println("我一路向北,离开有你的季节");
+        System.out.println("我一路向北,离开有你的季节");
+        System.out.println("我一路向北,离开有你的季节");
+        System.out.println("我一路向北,离开有你的季节");
+        System.out.println("我一路向北,离开有你的季节");
+        System.out.println("我一路向北,离开有你的季节");
+    }
+}
